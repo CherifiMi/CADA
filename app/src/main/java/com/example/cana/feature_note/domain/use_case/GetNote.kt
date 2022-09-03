a4c286d029c1e0aefa89e7a66ea5dd8bcf120d8a
CherifiMi <cherifi154949@gmail.com>
2022-09-03 20:50:32 +0100

    get note use case

--- app/src/main/java/com/example/cana/feature_note/domain/use_case/GetNote.kt
+++ app/src/main/java/com/example/cana/feature_note/domain/use_case/GetNote.kt
@@ -1,9 +1,12 @@
 package com.example.cana.feature_note.domain.use_case
 
+import com.example.cana.feature_note.domain.model.Note
 import com.example.cana.feature_note.domain.repository.NoteRepository
 
 class GetNote (
     private val repository: NoteRepository
 ) {
-
+    suspend operator fun invoke(id: Int): Note?{
+        return repository.getNotById(id)
+    }
 }
