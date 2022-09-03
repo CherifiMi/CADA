3eb921afa1986527f226dd3d01f02b66d840d297
CherifiMi <cherifi154949@gmail.com>
2022-09-03 20:39:51 +0100

    init add_edit_note screen

---
+++ app/src/main/java/com/example/cana/feature_note/ui/add_edit_note/AddEditNoteScreen.kt
@@ -0,0 +1,9 @@
+package com.example.cana.feature_note.ui.add_edit_note
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun AddEditNoteScreen(navController: NavHostController, color: Int) {
+
+}
