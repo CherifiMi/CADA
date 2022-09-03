95e766ffa7026e673114dbb31d3ef825c7dabe2e
CherifiMi <cherifi154949@gmail.com>
2022-09-03 20:50:42 +0100

    delete note use case

--- app/src/main/java/com/example/cana/feature_note/domain/use_case/DeleteNote.kt
+++ app/src/main/java/com/example/cana/feature_note/domain/use_case/DeleteNote.kt
@@ -1,9 +1,12 @@
 package com.example.cana.feature_note.domain.use_case
 
+import com.example.cana.feature_note.domain.model.Note
 import com.example.cana.feature_note.domain.repository.NoteRepository
 
 class DeleteNote (
     private val repository: NoteRepository
 ) {
-
+    suspend operator fun invoke(note: Note){
+        repository.deleteNote(note)
+    }
 }
