199af2f66ac48a74bd2f2fb2415bc2477474c8b4
CherifiMi <cherifi154949@gmail.com>
2022-09-03 20:22:52 +0100

    di

---
+++ app/src/main/java/com/example/cana/di/AppModule.kt
@@ -0,0 +1,41 @@
+package com.example.cana.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.cana.feature_note.data.NoteRepositoryImp
+import com.example.cana.feature_note.data.local.NoteDatabase
+import com.example.cana.feature_note.domain.repository.NoteRepository
+import com.example.cana.feature_note.domain.use_case.*
+import dagger.Provides
+import javax.inject.Singleton
+
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase{
+        return Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java,
+            NoteDatabase.Database_Name
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(dp: NoteDatabase): NoteRepository{
+        return NoteRepositoryImp(dp.notDao)
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCase {
+        return NoteUseCase(
+            getNote = GetNote(repository),
+            deleteNote = DeleteNote(repository),
+            addNote = AddNote(repository),
+            getNotes = GetNotes(repository)
+        )
+    }
+}
