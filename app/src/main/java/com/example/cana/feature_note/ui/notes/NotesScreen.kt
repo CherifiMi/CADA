938b8aa12da353a5e1b8b82a968b4840d4c7d6a5
CherifiMi <cherifi154949@gmail.com>
2022-09-03 20:39:17 +0100

    init note screen

---
+++ app/src/main/java/com/example/cana/feature_note/ui/notes/NotesScreen.kt
@@ -0,0 +1,9 @@
+package com.example.cana.feature_note.ui.notes
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun NotesScreen(navController: NavHostController) {
+
+}
