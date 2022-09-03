393eb4f3c8d42240ccb9c4cdea9fd9d7e886ad3c
CherifiMi <cherifi154949@gmail.com>
2022-09-03 20:39:05 +0100

    navigation

--- app/src/main/java/com/example/cana/MainActivity.kt
+++ app/src/main/java/com/example/cana/MainActivity.kt
@@ -2,10 +2,60 @@ package com.example.cana
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.cana.feature_note.ui.add_edit_note.AddEditNoteScreen
+import com.example.cana.feature_note.ui.notes.NotesScreen
+import com.example.cana.feature_note.ui.util.Screens
+import com.example.cana.theme.CANA
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContent {
+            CANA() {
+                Surface(
+                    color = MaterialTheme.colors.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screens.NotesList.name
+                    ) {
+                        composable(route = Screens.NotesList.name) {
+                            NotesScreen(navController)
+                        }
+                        composable(
+                            route =
+                            Screens.AddEditNote.name
+                                    + "?noteId={noteId}&noteColor={noteColor}",
+                            arguments = listOf(
+                                navArgument(
+                                    name = "noteId"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                                navArgument(
+                                    name = "noteColor"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                }
+                            )
+                        ) {
+                            val color = it.arguments?.getInt("noteColor") ?: -1
+                            AddEditNoteScreen(navController, color)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
