a7290b8a397359ee7eade79060844c8cae690ca3
CherifiMi <cherifi154949@gmail.com>
2022-09-04 19:58:22 +0100

    notes vm done

--- app/src/main/java/com/example/cana/feature_note/ui/notes/viewmodel/NotesViewModel.kt
+++ app/src/main/java/com/example/cana/feature_note/ui/notes/viewmodel/NotesViewModel.kt
@@ -1,13 +1,76 @@
 package com.example.cana.feature_note.ui.notes.viewmodel
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cana.feature_note.domain.model.Note
 import com.example.cana.feature_note.domain.use_case.NoteUseCase
+import com.example.cana.feature_note.domain.util.NoteOrder
+import com.example.cana.feature_note.domain.util.OrderType
+import com.example.cana.feature_note.ui.notes.states.NotesEvent
+import com.example.cana.feature_note.ui.notes.states.NotesState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class NotesViewModel @Inject constructor(
     private val noteUseCase: NoteUseCase
 ) : ViewModel() {
+    private val _state = mutableStateOf(NotesState())
+    val state: State<NotesState> = _state
 
+    private var recentlyDeletedNote: Note? = null
+
+    private var getNotesJob: Job? = null
+
+    init {
+        getNotes(NoteOrder.Date(OrderType.Des))
+    }
+
+    fun onEvent(event: NotesEvent) {
+        when (event) {
+            is NotesEvent.Order -> {
+                if (state.value.noteOrder::class == event.noteOrder::class &&
+                    state.value.noteOrder.orderType::class == event.noteOrder.orderType::class
+                ) {
+                    return
+                }
+                getNotes(event.noteOrder)
+            }
+            is NotesEvent.DeleteNote -> {
+                viewModelScope.launch {
+                    noteUseCase.deleteNote(event.note)
+                    recentlyDeletedNote = event.note
+                }
+            }
+            is NotesEvent.RestoreNote -> {
+                viewModelScope.launch {
+                    noteUseCase.addNote(recentlyDeletedNote ?: return@launch)
+                    recentlyDeletedNote = null
+                }
+            }
+            is NotesEvent.ToggleOrderSection -> {
+                _state.value = state.value.copy(
+                    isOrderSectionVisible = !state.value.isOrderSectionVisible
+                )
+            }
+        }
+    }
+
+    private fun getNotes(noteOrder: NoteOrder) {
+        getNotesJob?.cancel()
+        getNotesJob = noteUseCase.getNotes(noteOrder)
+            .onEach { notes ->
+                _state.value = state.value.copy(
+                    notes = notes,
+                    noteOrder = noteOrder
+                )
+            }
+            .launchIn(viewModelScope)
+    }
 }
