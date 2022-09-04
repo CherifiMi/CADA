6b48eddce10e31d6fe98a199a58d6756f3cae3c5
CherifiMi <cherifi154949@gmail.com>
2022-09-04 20:33:53 +0100

    new test tags

--- app/src/main/java/com/example/cana/core/util/TestTags.kt
+++ app/src/main/java/com/example/cana/core/util/TestTags.kt
@@ -1,6 +1,8 @@
 package com.example.cana.core.util
 
 object TestTags {
+    const val SORT = "Sort"
+    const val ADD = "Add"
     const val ORDER_SECTION = "ORDER_SECTION"
     const val TITLE_TEXT_FIELD = "TITLE_TEXT_FIELD"
     const val CONTENT_TEXT_FIELD = "CONTENT_TEXT_FIELD"
