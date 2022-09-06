0db9b19a665fe473fa0fbad09923980b09db77c6
CherifiMi <cherifi154949@gmail.com>
2022-09-06 06:09:47 +0100

    add edit screen comp state done

---
+++ app/src/main/java/com/example/cana/feature_note/ui/add_edit_note/states/NoteTextFieldState.kt
@@ -0,0 +1,7 @@
+package com.example.cana.feature_note.ui.add_edit_note.states
+
+data class NoteTextFieldState(
+    val text: String = "",
+    val hint: String = "",
+    val isHintVisible: Boolean = true
+)
