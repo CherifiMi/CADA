7631f8b3a8003a4c5eda3cac8686164c0557d93d
CherifiMi <cherifi154949@gmail.com>
2022-09-06 06:09:00 +0100

    add edit screen events done

---
+++ app/src/main/java/com/example/cana/feature_note/ui/add_edit_note/states/AddEditNoteEvent.kt
@@ -0,0 +1,12 @@
+package com.example.cana.feature_note.ui.add_edit_note.states
+
+import androidx.compose.ui.focus.FocusState
+
+sealed class AddEditNoteEvent{
+    data class EnteredTitle(val value: String): AddEditNoteEvent()
+    data class ChangeTitleFocus(val focusState: FocusState): AddEditNoteEvent()
+    data class EnteredContent(val value: String): AddEditNoteEvent()
+    data class ChangeContentFocus(val focusState: FocusState): AddEditNoteEvent()
+    data class ChangeColor(val color: Int): AddEditNoteEvent()
+    object SaveNote: AddEditNoteEvent()
+}
